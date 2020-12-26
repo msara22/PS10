ab813125c1d56e6ebbf4fedb319494901a83ade4
msara22 <72658217+msara22@users.noreply.github.com>
2020-12-26 20:50:54 +0600

    Update TipCalculator.java

--- TipCalculator.java
+++ TipCalculator.java
@@ -1,15 +1,3 @@
-package sample;
-
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-
-package TipCalculator;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -17,16 +5,18 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class TipCalculator extends Application {
+
     @Override
-    public void start(Stage stage) throws Exception{
+    public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("TipCalculator.fxml"));
+
         Scene scene=new Scene(root);
         stage.setTitle("Tip Calculator");
         stage.setScene(scene);
         stage.show();
-
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         launch(args);
     }
 }
